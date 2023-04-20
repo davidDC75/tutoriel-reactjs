c2f355ed76dedef47b5e3c02e8ea619ac407dcb2
David De Castro <segphault@gmail.com>
2023-04-20 18:53:27 +0200

    Début du tutoriel

--- js/main.jsx
+++ js/main.jsx
@@ -1,7 +1,65 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Hello from "./Hello.jsx";
+// import { ReactDOM } from "react-dom/client";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<Hello />);
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square />;
+    }
+
+    render() {
+      const status = 'Next player: X';
+
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  // ========================================
+
+  const root = createRoot(document.getElementById("root"));
+  root.render(<Game />);
