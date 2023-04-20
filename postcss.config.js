827f33fab6dccb92089491ba6c13a4ff0364542e
David De Castro <segphault@gmail.com>
2023-04-20 18:14:02 +0200

    Ajout d'autoprefixer pour postcss-loader

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: [
+        [
+            // https://www.npmjs.com/package/precss
+            // DEPRECATED
+            // "precss",
+            // https://www.npmjs.com/package/autoprefixer
+            "autoprefixer",
+            {
+                // Options
+            },
+        ],
+    ],
+};
