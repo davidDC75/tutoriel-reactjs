ea4eda10fa027bb7cfa50877add5ac0c89514817
David De Castro <segphault@gmail.com>
2023-04-20 19:32:43 +0200

    Affiche un X quand on clique sur une square du board

--- js/main.jsx
+++ js/main.jsx
@@ -3,10 +3,16 @@ import { createRoot } from "react-dom/client";
 // import { ReactDOM } from "react-dom/client";
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            value: null,
+        };
+    }
     render() {
       return (
-        <button className="square" onClick={ () => {alert('clic'); } }>
-          { this.props.value }
+        <button className="square" onClick={ () => { this.setState({value: 'X'}) } }>
+          { this.state.value }
         </button>
       );
     }
